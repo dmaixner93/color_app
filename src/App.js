80a7f21f03c3a0b70665fe90c1943528ec430430
daniel <dmaixner93@gmail.com>
2020-05-12 13:43:24 -0500

    Add ColorHelper to generatePalettes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Palette from './Palette.js';
 import seedColors from './seedColors';
+import { generatePalettes } from './colorHelpers.js'
 
 class App extends Component {
   render() {
+    console.log(generatePalettes(seedColors[4]));
     return (
       <Palette {...seedColors[4]}/>
     )
